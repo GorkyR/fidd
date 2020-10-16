8ee93f8aa800e78220844da2696b3499c275484c
Gorky Rojas <1gorky3rojas@gmail.com>
2020-10-16 15:09:27 -0400

    added project for testing feedmanager
    life quality +1

---
+++ FeedManagerTester/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FeedManagerTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var feed_manager = new FeedManager(Environment.ExpandEnvironmentVariables("Feeds"));
+            try { feed_manager.AddFeedAsync("tonsky.me").Wait(); } catch { }
+            try { feed_manager.AddFeedAsync("deathisbadblog.com").Wait(); } catch { }
+        }
+    }
+}
